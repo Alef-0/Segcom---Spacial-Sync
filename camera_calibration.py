856902ae0d3d3b4cdfa8c7d981d4d2e3404e07e5
Alef-0 <alefgabryel123@gmail.com>
2025-11-12 15:01:53 -0300

    Starting a new homography matrix

--- camera_calibration.py
+++ camera_calibration.py
@@ -1,25 +1,97 @@
 import cv2 as cv
 import numpy as np
+import rclpy
+from sensor_msgs.msg import Image
+from sensor_msgs.msg import CompressedImage
+from rclpy.node import Node
+from datetime import datetime
+import os
 
-class Checkerboard_Checker():
+
+class Checkerboard_Checker(Node):
     def __init__(self, dim = (6,9)):
+        super().__init__("CameraCalibration")
+        self.sub = self.create_subscription(CompressedImage, "Segcom/Camera/M/compressed", self.receive_image, 10)
         self.dimensions = dim
         self.objp = np.zeros((1, dim[0] * dim[1], 3), np.float32)
         self.objp[0,:,:2] = np.mgrid[0:dim[0], 0:dim[1]].T.reshape(-1, 2)
         self.criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
+        self.folder = datetime.now()
+        self.i = 0; os.makedirs(f"{self.folder}", exist_ok=True)
+
         self.objpoints = []
         self.imgpoints = []
 
-        self.intrinsic_matrix = None
-        self.distortion_matrix = None
+        self.intrinsic_matrix : np.ndarray  = None
+        self.distortion_matrix : np.ndarray = None
+        self.transformation_matrix : np.ndarray = None
+
+        self.img_white = np.full((1080, 1920, 3), 255, dtype=np.uint8)
+
+    def draw_debug_corners(self, corners : np.ndarray):
+        current = self.img_white.copy()
+        
+        # Drawing the current
+        if corners is not None: 
+            list_corners = corners.reshape(-1, 2).astype(np.int32)
+            for x, y in list_corners: 
+                cv.circle(current, (x,y), 8, (255,0,0), -1)
+            # cv.fillPoly(current, [list_corners], (100,0,0, 255), 4)
+
+        return current
+                
+
+
+    def receive_image(self, msg : Image):
+        # COMPRESSED IMAGE
+        img = cv.imdecode(np.frombuffer(msg.data, np.uint8), cv.IMREAD_COLOR)
+        img = cv.resize(img, (1920, 1080), interpolation=cv.INTER_CUBIC)
+        img, corners = checker.look_for_checkerboard(img)
+
+        seeing = self.draw_debug_corners(corners)
+
+        # key checking
+        cv.imshow("CAMERA", cv.resize(img, (640, 480)))
+        cv.imshow("SEEING", cv.resize(seeing, (1280, 720)))
+
+        key = chr(cv.waitKey(1) & 0xFF)
+        if key == "p": 
+            if corners is  not None:
+                list_corners = corners.reshape(-1, 2).astype(np.int32)
+                for x, y in list_corners: 
+                    cv.circle(self.img_white, (x,y), 8, (0,0,255), -1)
+                checker.objpoints.append(checker.objp)
+                checker.imgpoints.append(corners)
+                print(f"Temos mais uma imagem ({self.i})"); self.i+=1
+                cv.imwrite(f"{self.folder}/image_{self.i:04}.jpeg", img)
+        if key == "r" or key == "q":
+            checker.calibrate_camera(1920, 1080)
+            self.print_json_like()
+            cv.imwrite(f"{self.folder}/COVERAGE.jpeg", seeing)
+        if key == "q": rclpy.shutdown()
+
+        # if key == 'p':
+        #     print("TO DO")
+
+    def print_json_like(self):
+        text = ""
+        print("--------------")
+        new = "{\n"; text += new; print(new, end="")
+        new = f"\t\"Intrinsic\": {self.intrinsic_matrix.tolist()},\n"; text += new; print(new, end="")
+        new = f"\t\"Distortion\": {self.distortion_matrix.tolist()},\n"; text += new; print(new, end="")
+        # new = f"\t\"Transformation\": {self.transformation_matrix.tolist()},\n"; text += new; print(new, end="") # Always better from scratch
+        new = f"\t\"ret\": {self.ret}\n"; text += new; print(new, end="")
+        new = "}"; text += new; print(new, end="")
+        print("--------------")
+        with open("most_current.json", "w") as file: file.write(text)
 
     def look_for_checkerboard(self, img):
         gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
         ret, corners = cv.findChessboardCorners(gray, self.dimensions, 
                             cv.CALIB_CB_ADAPTIVE_THRESH + cv.CALIB_CB_FAST_CHECK + cv.CALIB_CB_NORMALIZE_IMAGE)
         if ret:
-            new_corners = cv.cornerSubPix(gray, corners, (11,11),(-1,-1), self.criteria)
+            new_corners : np.ndarray = cv.cornerSubPix(gray, corners, (11,11),(-1,-1), self.criteria)
             new_img = cv.drawChessboardCorners(img, self.dimensions, new_corners, ret)
         else: new_img = img; new_corners = None
 
@@ -31,28 +103,16 @@ class Checkerboard_Checker():
         self.intrinsic_matrix = mtx
         self.distortion_matrix = dist        
         self.transformation_matrix = new_camera_matrix
+        self.ret = ret
 
-def main():
-    cam = cv.VideoCapture(0)
+if __name__ == "__main__": 
+    
+    rclpy.init()
     checker = Checkerboard_Checker()
-    while True:
-        ret, img = cam.read()
-        if not ret: break
-
-        img, corners = checker.look_for_checkerboard(img)
-        cv.imshow("CAMERA", img)
-        
-        # key checking
-        key = chr(cv.waitKey(1) & 0xFF)
-        if key == "q": break
-        if key == "a": 
-            if corners is  None: continue
-            checker.objpoints.append(checker.objp)
-            checker.imgpoints.append(corners)
-            print("Temos mais uma imagem")
-        if key == "r":
-            checker.calibrate_camera(*img.shape[1:])
-        
+    
+    try: rclpy.spin(checker)
+    except: pass
+    
+    if rclpy.ok(): rclpy.shutdown()
     cv.destroyAllWindows()
-
-if __name__ == "__main__": main()
+    # main()
--- camera_mapping.py
+++ camera_mapping.py
@@ -1,18 +1,42 @@
 import numpy as np
 import cv2 as cv
+import json
+import math
+
+import time
+from aux_files import Files
+from aux_graph import Graph
+
+# Correção a ser aplicada para os valores
+CORRECTION_Y = 0.4
+CORRECTION_X = 0.1
+SLOPE = math.tan(math.radians(30))
 
 class Transformation():
-    def __init__(self, intrinsic : np.ndarray, distortion : np.ndarray, homography : np.ndarray):
-        self.k = intrinsic
-        self.d = distortion
-        self.h = homography
+    def __init__(self, file, intrinsic : np.ndarray = None, distortion : np.ndarray = None, homography : np.ndarray = None):
+        if file is not None:
+            everything : dict = json.load(open(file, 'r'))
+            self.k = np.array(everything["Intrinsic"], dtype = np.float64) if "Intrinsic" in everything else None
+            self.d = np.array(everything["Distortion"][0], dtype = np.float64) if "Distortion" in everything else None # For some reason it's a list
+            self.opt_matrix, _ = cv.getOptimalNewCameraMatrix(self.k, self.d, (1920, 1080), 1, (1920, 1080))
 
-        self.fx, self.fy = self.k[0,0], self.k[1,1]
-        self.cx, self.cy = self.k[0,2], self.k[1,2]
+            self.fx, self.fy = self.k[0,0], self.k[1,1]
+            self.cx, self.cy = self.k[0,2], self.k[1,2]
+            self.k1, self.k2, self.p1, self.p2, self.k3 = self.d
 
-        self.k1, self.k2, self.p1, self.p2, self.k3 = self.d
+            print(self.k, self.d, self.opt_matrix)
 
-        self.opt_matrix, self.roi = cv.getOptimalNewCameraMatrix(self.k, self.d, (1920, 1080), 1, (1920, 1080))
+        else:
+            self.k = intrinsic
+            self.d = distortion
+            self.h = homography
+
+            self.fx, self.fy = self.k[0,0], self.k[1,1]
+            self.cx, self.cy = self.k[0,2], self.k[1,2]
+            self.k1, self.k2, self.p1, self.p2, self.k3 = self.d
+            self.opt_matrix, _ = cv.getOptimalNewCameraMatrix(self.k, self.d, (1920, 1080), 1, (1920, 1080))
+
+            print(self.opt_matrix)
 
     def undistort_image(self, img : cv.Mat): return cv.undistort(img, self.k, self.d, None, self.opt_matrix)
 
@@ -36,15 +60,15 @@ class Transformation():
         points_distorted[:, 1] = points[:, 1]  * radial + self.p1 *(r2 + 2*points[:, 1]**2) + 2 * self.p2 * points[:, 0] * points[:, 1]
 
         # Convert back to pixel coordinates
-        distorted_pixels = np.zeros_like(points_distorted)
-        distorted_pixels[:, 0] = points_distorted[:, 0] * self.fx + self.cx
-        distorted_pixels[:, 1] = points_distorted[:, 1] * self.fy + self.cy
+        complex_points = np.zeros_like(points_distorted)
+        complex_points[:, 0] = points_distorted[:, 0] * self.fx + self.cx
+        complex_points[:, 1] = points_distorted[:, 1] * self.fy + self.cy
         
         simple_points = np.zeros_like(points)
         simple_points[:,0] = points[:,0] * self.fx + self.cx
         simple_points[:,1] = points[:,1] * self.fy + self.cy
 
-        return distorted_pixels, simple_points
+        return complex_points, simple_points
 
     def radar_to_distorted(self, points : np.ndarray):
         # Apply Homogeneous matrix transformation [Same thing as cv.perspective transform]
@@ -57,53 +81,77 @@ class Transformation():
         distorted_complex, distorted_simple = self.distort_points(points_normalized) # K and D
         
         return undistorted_pixels, distorted_complex, distorted_simple
+    
+    def generate_homography(self, files : Files):
+        for num in range(files.first, files.last):
+            image, points = files.get(num)
+            if image is None or not points.any(): continue
 
-import time
-from aux_files import Files
-from aux_graph import Graph
+            # Undistort image
+            distorted = image.copy()
+            undistorted = mapping.undistort_image(image)
+                
+            cv.imshow("CROPPED", cv.resize(undistorted, (1280, 720)))
+            cv.imshow("ORIGINAL", cv.resize(distorted, (1280, 720)))
+            key = cv.waitKey(16) & 0xFF
+
+    def radar_to_undistorted(self, x : np.ndarray, y : np.ndarray):
+        # Delimitar Y
+        y_normalized = np.minimum(y / 10, 1)  # Normalizar e ao mesmo tempo ter um offset minimo
+        y_image_points = 1080 - ((CORRECTION_Y + (1 - CORRECTION_Y) * y_normalized) * 540) # Pegar do de baixo
+
+        # Delimitar X
+        x_image_points = [] # Needs to be based around the distance avaiable
+        for dist, radius in zip(x,y):
+            max_x = radius / SLOPE
+            x_normalized = dist / max_x
+            print(max_x, x_normalized)
+            result = 960 + x_normalized * 960
+            x_image_points.append(result)
+            # print(x_normalized, result)
+        
+        return np.column_stack([x_image_points, y_image_points])
+
+
 
 if __name__ == '__main__':
-    MATRIX = np.array([
-	   [1680.24,    0,          644.38],
-       [0,          1674.94,    360.14],
-       [0,          0,          1]]
-	   , dtype=np.float64)
-    DISTORTION = np.array([-0.586, -1.263, 0.034, -0.025, 2.036])
-
+    MATRIX = np.array([[6221.92422097575, 0.0, 907.0708040584582], [0.0, 7620.661949881767, 120.96692648284439], [0.0, 0.0, 1.0]], dtype=np.float64)
+    DISTORTION = np.array([-8.201663966549852, 93.00752542264131, 0.22430706498995545, 0.03268512101141362, -607.527225839154], dtype=np.float64)
     HOMOGRAPH = np.array([
                             [     42.819,      152.72,      1015.6],
                             [    -185.61,     59.863,      747.48],
                             [    -0.3605,      0.1478,           1]
                             ])
 
-
     files = Files()
     graph = Graph()
-    mapping = Transformation(MATRIX, DISTORTION, HOMOGRAPH)
+    # mapping = Transformation(None, MATRIX, DISTORTION, HOMOGRAPH)
+    mapping = Transformation("second_attempt.json")
+    # mapping.generate_homography(files)
 
     for num in range(files.first, files.last):
         image, points = files.get(num)
         if image is None or not points.any(): continue
+        graph.show_points(points[:, 0], points[:, 1], len(points) * [(0,200,0)])
 
         distorted = image.copy()
         undistorted = mapping.undistort_image(image)
+        points_undistorted = mapping.radar_to_undistorted(points[:,0], points[:,1])
+        print(points_undistorted)
 
-        und_points, dist_complex, dist_simple = mapping.radar_to_distorted(points)
-        for x, y in dist_complex:
-            if 0 < x < 1920 and 0 < y < 1080:
-                cv.circle(distorted, (int(x), int(y)), 12, (255,0,0), -1)
-        for x, y in dist_simple:
-            if 0 < x < 1920 and 0 < y < 1080:
-                cv.circle(distorted, (int(x), int(y)), 12, (0,255,0), -1)
-        for x, y in und_points:
+        # und_points, dist_complex, dist_simple = mapping.radar_to_distorted(points)
+        # for x, y in dist_complex:
+        #     if 0 < x < 1920 and 0 < y < 1080:
+        #         cv.circle(distorted, (int(x), int(y)), 12, (255,0,0), -1)
+        # for x, y in dist_simple:
+        #     if 0 < x < 1920 and 0 < y < 1080:
+        #         cv.circle(distorted, (int(x), int(y)), 12, (0,255,0), -1)
+        for x, y in points_undistorted:
             cv.circle(undistorted, (int(x), int(y)), 12, (0,0,255), -1)
                 
-
-
-        
         cv.imshow("CROPPED", cv.resize(undistorted, (1280, 720)))
         cv.imshow("ORIGINAL", cv.resize(distorted, (1280, 720)))
-        key = cv.waitKey(16) & 0xFF
+        key = cv.waitKey(40) & 0xFF
         
         match key:
             case 113: break # Q
@@ -112,3 +160,4 @@ if __name__ == '__main__':
                         key = cv.waitKey(16) & 0xFF
                         match key:
                             case 112: break # Q
+            case _: print(key)
