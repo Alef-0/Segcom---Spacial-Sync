844822db166e6b2e7862f17b03dc0d37e74ec15b
Alef-0 <alefgabryel123@gmail.com>
2025-12-03 10:14:50 -0300

    Intrinsic not working, but rest ok

---
+++ file_visualizer.py
@@ -0,0 +1,52 @@
+import numpy as np
+import cv2 as cv
+from aux_files import Files
+from aux_graph import Graph
+from aux_vision import Vision
+from math import radians
+from gui_matrix import windows_control
+import threading
+import multiprocessing
+
+def create_60_slope_points(): 
+    x = np.arange(0, 6, 0.01); 
+    y = x * radians(30);
+    left = np.vstack([-x, y]) ; right = np.vstack([x,y])
+    return np.hstack([left, right]).T
+    
+def radar_to_image(homography_matrix, points, image, color):
+    radar_homogenous = np.hstack([points, np.ones((points.shape[0],1))])
+    radar_homogenous = (homography_matrix @ radar_homogenous.T).T
+    final_points = radar_homogenous[:,:2] / radar_homogenous[:,2:3]
+
+    for x, y in final_points: cv.circle(image, (int(x), int(y)), 3, color, 5, -1)
+
+def visualize_everything(menu : windows_control, stop : threading.Event):
+    files = Files()
+    vision = Vision()
+    graph = Graph()
+    slope = create_60_slope_points()
+    dist = np.array([0,0,0,0,0], dtype=np.float64)
+
+    # Código de visualização
+    for num in range(files.first + 100, files.last):
+        image, points = files.get(num)
+        if image is None or not points.any(): continue
+
+        image = cv.undistort(image, menu.intrinsic, dist)
+        radar_to_image(menu.extrinsic, slope, image, (255,255,255)) # Drawing o the image
+        radar_to_image(menu.extrinsic, points, image, (0,0,255))
+
+        cv.imshow("CROPPED", cv.resize(image, (1280, 720)))
+        key = cv.waitKey(60) & 0xFF
+        if key in (27, ord("q")): break
+        elif stop.is_set(): break
+
+    cv.destroyAllWindows()
+
+if __name__ == "__main__":
+    menu = windows_control()
+    stop = threading.Event()
+    th = threading.Thread(target=visualize_everything, args=(menu,stop), daemon=True); th.start()
+    menu.run_window()
+    stop.set(); th.join()
--- gui_matrix.py
+++ gui_matrix.py
@@ -5,13 +5,16 @@ from tkinter import Widget
 def prnt_num(num):
     return f"{num:06.01f}"
 
-class values_slider:
+class windows_control:
     def __init__(self):
-        # sg.theme("SystemDefaultForReal")
-        
-
-        self.intrinsic = np.array([[1,0,0], [0,1,0], [0,0,1]], dtype = np.float64)
-        self.extrinsic = np.array([[1,0,0], [0,1,0], [0,0,1]], dtype = np.float64)
+        self.intrinsic = np.array([
+            [1,0,0], 
+            [0,1,0], 
+            [0,0,1]], dtype = np.float64)
+        self.extrinsic = np.array([
+            [960,0,960], 
+            [0,-540,1080], 
+            [0,0,1]], dtype = np.float64)
         self.selected = None
 
         self.layout = [
@@ -22,6 +25,7 @@ class values_slider:
         self.window = sg.Window("SLIDER", self.layout, finalize=True)
         self.update_all_buttons()
         self.window.hide()
+        self.previous_num = 0.0
 
     def update_all_buttons(self):
         for x in [0,1,2]:
@@ -45,9 +49,11 @@ class values_slider:
 
     def create_slider_layout(self):
         return sg.Frame("Value Adjust", [
-            [sg.Button("-100", key="btn_1"), sg.Button("-10", key="btn_2"), sg.Button("-1", key="btn_3"), sg.Button("-0.1", key="btn_4"),
-             sg.Button("+0.1", key="btn_5"), sg.Button("+1", key="btn_6"), sg.Button("+10", key="btn_7"), sg.Button("+100", key="btn_8")],
-             [sg.Slider((-1000, 1000), (0), (0.01), orientation='h', expand_x=True, key="SLIDER_VALUE", enable_events=True)]
+            [ sg.Button("-1", key="btn_10"), sg.Button("-0.1", key="btn_1"),  sg.Button("-0.01", key="btn_2"),   sg.Button("-0.001", key="btn_3"),  
+            sg.Button("+0.001", key="btn_4"), sg.Button("+0.01", key="btn_5"), sg.Button("+0.1", key="btn_6"), sg.Button("+1", key="btn_11")],
+             [sg.Slider((-1080, 1080), (0), (0.01), orientation='h', expand_x=True, key="slider", enable_events=True, disable_number_display=True), 
+              sg.Input("0.0", key="input_text", size=(6,20)), sg.Ok(key="input_ok")
+              ]
         ], title_location=sg.TITLE_LOCATION_BOTTOM, key="SLIDER")
 
     def create_cases_selector(self):
@@ -60,42 +66,52 @@ class values_slider:
             [sg.Button("<<", key="con_prev"), sg.Button("||", key="con_pause"), sg.Button(">", key="con_play"), sg.Button(">>", key="con_next")]
         ], title_location= sg.TITLE_LOCATION_TOP)
 
-
     def value_from_matrix(self, event, update = False, value = None):
         tipo, x, y = event[7:] # Do tipo choose_xxxx
         x = int(x); y = int(y)
         sel = self.intrinsic if tipo == "I" else self.extrinsic
-        if update: sel[x,y] = value
+        if update: sel[x,y] = value; self.update_all_buttons()
         return sel[x,y]
 
     def run_window(self):
         self.window.UnHide()
+
         while True:
             event, values = self.window.read(1)
             
             # Cases
             if event == sg.TIMEOUT_EVENT: pass
             elif event == sg.WINDOW_CLOSED: break
-            elif event == "SLIDER_VALUE":       # Evento do slider
-                if self.selected: 
-                    self.value_from_matrix(self.selected, True, values[event])
-                    self.update_all_buttons()
+            elif event == "slider":       # Evento do slider
+                self.window['input_text'].update(values[event])
+                if self.selected: self.value_from_matrix(self.selected, True, values[event])
+                self.previous_num = values[event]
             elif event.startswith("choose_"):   # Selecionar o valor das matrizes
                 if self.selected: self.window[self.selected].update(disabled = False)
                 self.selected = event
-                self.window["SLIDER_VALUE"].update(self.value_from_matrix(event))
+                value = self.value_from_matrix(event)
+                self.window["slider"].update(value)
+                self.window["input_text"].update(value)
                 self.window[event].update(disabled = True)
             elif event.startswith("btn_"):      # Mudar o valor do slider por botão
-                new_value = min(max(-1000,values["SLIDER_VALUE"] + float(self.window[event].ButtonText)),1000)
-                self.window["SLIDER_VALUE"].update(new_value)
-                if self.selected: 
-                    self.value_from_matrix(self.selected, True, new_value)
-                    self.update_all_buttons()
+                text = self.window[event].ButtonText
+                new_value = 0.001 if text == "zero" else float(values["input_text"]) + float(text)
+                self.window["slider"].update(new_value)
+                self.window["input_text"].update(new_value)
+                if self.selected: self.value_from_matrix(self.selected, True, new_value)
+                self.previous_num = new_value
             elif event.startswith("sel_"):
                 if event.endswith("fx"): self.window["choose_I00"].click()
                 if event.endswith("fy"): self.window["choose_I11"].click()
                 if event.endswith("cx"): self.window["choose_I02"].click()
                 if event.endswith("cy"): self.window["choose_I12"].click()
+            elif event == "input_ok":
+                try: 
+                    value = float(values["input_text"])
+                    self.window["slider"].update(float(value))
+                    if self.selected: self.value_from_matrix(self.selected, True, value); 
+                    self.previous_num = value
+                except: self.window["input_text"].update(self.previous_num)
             else: print(event, values)
             # print(values)
 
@@ -103,5 +119,5 @@ class values_slider:
         self.window.close()
 
 if __name__ == "__main__":
-    slider = values_slider()
+    slider = windows_control()
     slider.run_window()
